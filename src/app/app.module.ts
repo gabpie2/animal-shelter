26a7d99da9684a77bde74b9ebc2f2e98866e02d3
gabpie2 <gabi9000@o2.pl>
2023-01-14 10:30:29 +0100

    commit 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-
 import { MainPageModule } from './features/main-page/main-page.module';
  
 //import Pipe i w deklaracji, teraz jest w DogModule
